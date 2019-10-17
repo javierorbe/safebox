d3fc87eb836d16264857018688140bde3b19debf
Javier Orbe <javier.orbe@opendeusto.es>
2019-10-17 22:09:58 +0200

    Small change on LeafItem.

--- client/src/main/java/deusto/safebox/client/datamodel/LeafItem.java
+++ client/src/main/java/deusto/safebox/client/datamodel/LeafItem.java
@@ -2,6 +2,10 @@ package deusto.safebox.client.datamodel;
 
 import java.time.LocalDateTime;
 
+/**
+ * Super type for items contained in a folder.
+ * All items (except for folders) should inherit this class.
+ */
 public abstract class LeafItem extends Item {
 
     private Folder folder;
@@ -11,7 +15,20 @@ public abstract class LeafItem extends Item {
         this.folder = folder;
     }
 
+    /**
+     * Returns the folder where the item is.
+     *
+     * @return a reference of the folder.
+     */
     public Folder getFolder() {
         return folder;
     }
+
+    /**
+     * Returns a property (string, number...) of the item.
+     *
+     * @param index the index of the property.
+     * @return the property in the specified index.
+     */
+    public abstract Object getProperty(int index);
 }
