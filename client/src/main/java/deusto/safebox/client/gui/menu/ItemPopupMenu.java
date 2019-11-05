a54b44cd74bed3d36912d4dd79a4cdde8409ae1f
Javier Orbe <javier.orbe@opendeusto.es>
2019-11-05 15:51:13 +0100

    Add ItemPopupMenu.

---
+++ client/src/main/java/deusto/safebox/client/gui/menu/ItemPopupMenu.java
@@ -0,0 +1,29 @@
+package deusto.safebox.client.gui.menu;
+
+import static deusto.safebox.client.util.IconManager.IconType;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+public class ItemPopupMenu extends JPopupMenu {
+
+    protected ItemPopupMenu() {
+        final JMenuItem newItem = new JMenuItem("New item", IconType.NEW_FILE_12.getAsIcon());
+        final JMenuItem editItem = new JMenuItem("Edit item", IconType.NEW_FILE_12.getAsIcon());
+        final JMenuItem deleteItem = new JMenuItem("Delete item", IconType.NEW_FILE_12.getAsIcon());
+
+        final JMenuItem copyUsername = new JMenuItem("Copy username", IconType.NEW_FILE_12.getAsIcon());
+        final JMenuItem copyPassword = new JMenuItem("Copy password", IconType.NEW_FILE_12.getAsIcon());
+
+        newItem.addActionListener(e -> { /* TODO */ });
+        editItem.addActionListener(e -> { /* TODO */ });
+        deleteItem.addActionListener(e -> { /* TODO */ });
+
+        add(newItem);
+        add(editItem);
+        add(deleteItem);
+        addSeparator();
+        add(copyUsername);
+        add(copyPassword);
+    }
+}
